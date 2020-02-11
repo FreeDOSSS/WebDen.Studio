e1c99b3ff7b59eb7f3f2b78747a61e50b55d5dbb
FreeDOSSS <dima15521@gmail.com>
2020-02-11 16:54:07 +0200

    mobile style

--- build-utils/webpack.development.js
+++ build-utils/webpack.development.js
@@ -1,36 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = env => ({
-  devtool: 'cheap-module-source-map',
+  devtool: "cheap-module-source-map",
   output: {
-    publicPath: '/'
+    publicPath: "/"
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"]
       },
+
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader']
+        use: ["style-loader", "css-loader", "postcss-loader"]
       }
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: './src/index.html', minify: false })
+    new HtmlWebpackPlugin({ template: "./src/index.html", minify: false })
   ],
   devServer: {
-    contentBase: path.resolve(__dirname, '../src'),
+    contentBase: path.resolve(__dirname, "../src"),
     watchContentBase: true,
-    publicPath: '/',
+    publicPath: "/",
     hot: false,
     historyApiFallback: true,
     noInfo: true,
     quiet: true,
-    stats: 'errors-only',
-    clientLogLevel: 'warning',
+    stats: "errors-only",
+    clientLogLevel: "warning",
     compress: true,
     open: true,
     port: env.port
